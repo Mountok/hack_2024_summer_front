81f782e87b0c450e141691461097a97f753c41e7
admin <admin@MacBook-Pro13-Mountok.local>
2024-11-13 08:32:34 +0300

    -add: modal fix conf

--- settings.js
+++ settings.js
@@ -3,10 +3,10 @@
 //     PORT: "127.0.0.1:8080",
 //     HOST: "http://127.0.0.1:8080"
 // }
-const Settings = {
-    PORT: "172.20.10.2:8080",
-    HOST: "http://172.20.10.2:8080"
-}
+// const Settings = {
+//     PORT: "172.20.10.2:8080",
+//     HOST: "http://172.20.10.2:8080"
+// }
 // const Settings = {
 //     PORT: "10.231.231.232:8080",
 //     HOST: "http://10.231.231.232:8080"
@@ -29,8 +29,8 @@ const Settings = {
 
 
 // https://itupapi-production.up.railway.app/
-// const Settings ={
-//     PORT: "ggkitapicourses-production.up.railway.app",
-//     HOST: "https://ggkitapicourses-production.up.railway.app"
-// }
+const Settings ={
+    PORT: "ggkitapicourses-production.up.railway.app",
+    HOST: "https://ggkitapicourses-production.up.railway.app"
+}
 export default Settings
