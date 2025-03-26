eeafaaec2b2ba34a0b2348be1fe8dd45e7425d0b
Mountok <themountok@gmail.com>
2025-03-26 15:47:46 +0300

    -upd: host

--- settings.js
+++ settings.js
@@ -4,8 +4,8 @@
 //     HOST: "http://127.0.0.1:8080"
 // }
 const Settings = {
-    PORT: "172.20.10.2:8080",
-    HOST: "http://172.20.10.2:8080"
+    PORT: "193.227.240.114:8080",
+    HOST: "http://193.227.240.114:8080"
 }
 
 // https://itupapi-production.up.railway.app/
